78ef29cfdfd3db8e32ed8cfae65531760a9587d4
OUSMANE DAOU <ousmanedaou@live.fr>
2023-02-15 09:20:46 +0530

    Deletion of undesirable files in administrator panel

--- src/Controller/AdministratorController.php
+++ src/Controller/AdministratorController.php
@@ -8,12 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdministratorController extends AbstractController
 {
-    #[Route('/administrator', name: 'app_administrator')]
+    #[Route('/admin', name: 'app_administrator')]
     public function index(): Response
     {
         return $this->render('administrator/index.html.twig', [
             'controller_name' => 'AdministratorController',
             'page_title' => 'Administrator Panel',
         ]);
+    }
+    #[Route('/settings', name: 'setting')]
+    public function settings()
+    {
+        return $this->render('administrator/settings.html.twig');
     }
 }
